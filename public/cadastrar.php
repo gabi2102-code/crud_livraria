354470c09b15a6cc19279581463d6c28346a93b8
gabb <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-08-12 21:13:22 -0300

    add- adicionei o Prepared Statements na pagina de cadastrar

--- public/cadastrar.php
+++ public/cadastrar.php
@@ -6,9 +6,18 @@ $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
 
-$sql = "INSERT INTO livros (titulo,autor,ano) VALUES ('$titulo','$autor','$ano')";
+$stmt = $conexao->prepare(
+    "INSERT INTO livros (titulo, autor, ano)
+     VALUES (?, ?, ?)"
+);
 
-mysqli_query($conexao, $sql);
+$stmt->bind_param(
+    "ssi",
+    $titulo,
+    $autor,
+    $ano
+);
 
+$stmt->execute();
 header("Location: ../index.php");
 ?>
