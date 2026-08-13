0acbef88e6e33cc41b18240306331ac69d5606b5
gabb <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-08-12 21:16:49 -0300

    add- adicionei o Prepared Statements na pagina de atualização

--- public/atualizar.php
+++ public/atualizar.php
@@ -7,7 +7,19 @@ $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
 
-$sql = "UPDATE livros SET titulo='$titulo',autor='$autor',ano='$ano' WHERE id = '$id'";
+$stmt = $conexao->prepare(
+    "UPDATE livros
+     SET titulo = ?, autor = ?, ano = ?
+     WHERE id = ?"
+);
 
-mysqli_query($conexao, $sql);
+$stmt->bind_param(
+    "ssii",
+    $titulo,
+    $autor,
+    $ano,
+    $id
+);
+
+$stmt->execute();
 header("Location: ../index.php");
